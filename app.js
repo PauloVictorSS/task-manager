5fbb2423c4383cd72f5d2d19c817fa9dddfa776e
Paulo Victor <p248438@dac.unicamp.br>
2022-03-21 17:50:25 -0300

    Adição das rotas que serão utilizadas

--- app.js
+++ app.js
@@ -10,8 +10,28 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => {
 
-    console.log("Estamos funcionando");
-})
+    res.send("Ver todas as tarefas");
+});
+
+app.post("/", (req, res, next) => {
+
+    res.send("Adicionando uma tarefa");
+});
+
+app.get("/task/:id", (req, res, next) => {
+
+    res.send("Mostrando uma tarefa");
+});
+
+app.patch("/task/:id", (req, res, next) => {
+
+    res.send("Atualizando uma tarefa");
+});
+
+app.delete("/task/:id", (req, res, next) => {
+
+    res.send("Deletando uma tarefa");
+});
 
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
